559ce3684c67ffc176632d37fa75852d70f8b57c
fardin-ahsan <fardin.ahsan10@gmail.com>
2020-11-12 01:19:21 +0600

    Updated names, added a folder and a text file

--- version_2/AI.java
+++ version_2/AI.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author INSERT YOUR NAME(S) HERE
+ * @author Fardin Ahsan Sourav
  */
 
 public interface AI {
--- version_2/Move.java
+++ version_2/Move.java
@@ -3,7 +3,7 @@
  *
  * STUDENTS: Nothing to change here.
  *
- * @author Andrew Vardy
+ * @author Fardin Ahsan Sourav
  */
 
 public class Move {
